7a413249b43aedeffc83cb35dbb8968417338193
Andreas Heine <andreas_heine@gmx.net>
2026-03-10 17:37:39 +0100

    change url

--- i3x_server/config/settings.py
+++ i3x_server/config/settings.py
@@ -5,7 +5,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_prefix="I3X_", env_file=".env", extra="ignore")
 
-    opcua_endpoint: str = Field(default="opc.tcp://localhost:4840")
+    opcua_endpoint: str = Field(default="opc.tcp://opcua.umati.app:4843")
     opcua_security_mode: str = Field(default="None")
     model_refresh_interval_seconds: int = Field(default=60, ge=0)
     log_level: str = Field(default="INFO")
